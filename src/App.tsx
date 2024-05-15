9ffed7a9e6ee4ae186a64f10595d9869ac594cec
Harold Portocarrero <harold.port@gmail.com>
2024-05-14 20:45:36 -0500

    Add button component exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <Alert>
         Hello <span>world</span>
       </Alert>
+      <Button color="danger" onClick={() => console.log("Clicked")}>
+        Press me
+      </Button>
     </div>
   );
 }
