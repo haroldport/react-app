07739a49b46e68bbfb10d7e769a1a2632ec7a7c7
Harold Portocarrero <harold.port@gmail.com>
2024-05-15 12:13:41 -0500

    Styling with vanilla css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ["Guayaquil", "Quito", "Cuenca"];
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>
-        Press me
-      </Button>
+      <ListGroup
+        items={items}
+        heading="Ciudades"
+        onSelectItem={(item) => console.log(item)}
+      />
     </div>
   );
 }
