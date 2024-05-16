74335db1cba0afa1270889e239ba7fdbb0e400dd
Harold Portocarrero <harold.port@gmail.com>
2024-05-16 18:39:52 -0500

    Building form: access input fields with useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus,
-        dolorem ut. Qui tenetur rerum soluta error. Atque, eaque incidunt. Illum
-        quidem beatae cum hic nemo incidunt quam corrupti necessitatibus quae
-        suscipit, totam consectetur odio eaque, quo laborum ea rerum reiciendis
-        recusandae deleniti magnam modi. Deleniti fugit ipsam explicabo magni.
-        Delectus sed vitae nobis ab animi, atque culpa voluptatum. Architecto
-        impedit autem, aliquam velit officiis quidem? Minus delectus quae illum,
-        suscipit, officia maiores adipisci possimus, doloremque libero id beatae
-        omnis saepe necessitatibus eius provident eum dicta ut perspiciatis odio
-        animi vitae facilis. Quas, autem deleniti. Et nam similique earum
-        distinctio magni?
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
