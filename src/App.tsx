8a8032a1c24f82fe246f9f36cbaef52b61e82285
Harold Portocarrero <harold.port@gmail.com>
2024-05-16 17:51:19 -0500

    Exercise 2: Updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
   });
 
   const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Bob" } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
   };
 
   return (
